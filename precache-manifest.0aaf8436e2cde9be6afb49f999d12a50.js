bcdbea8875b45c774bc78bdcdd4c74e59be13df0
rem029 <elawrenceponce@gmail.com>
2020-08-23 09:13:06 +0300

    Updates

---
+++ precache-manifest.0aaf8436e2cde9be6afb49f999d12a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97f12861c272c87778963b082820490",
+    "url": "/ImageSlider/index.html"
+  },
+  {
+    "revision": "6ae598d208d9ceff9744",
+    "url": "/ImageSlider/static/css/main.b297d921.chunk.css"
+  },
+  {
+    "revision": "6ce6ac7bd0d2a6255106",
+    "url": "/ImageSlider/static/js/2.bcff89f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ImageSlider/static/js/2.bcff89f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae598d208d9ceff9744",
+    "url": "/ImageSlider/static/js/main.d95fc39e.chunk.js"
+  },
+  {
+    "revision": "60c877f6c7cfbff17f5e",
+    "url": "/ImageSlider/static/js/runtime-main.c29cbb27.js"
+  }
+]);
